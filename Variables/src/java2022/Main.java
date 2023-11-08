def6d887a90aecad8c5409f17d5e72b526cb1ccf
Eren Beken <erenbeken42@gmail.com>
2023-11-08 20:28:49 +0300

    Variables

---
+++ Variables/src/java2022/Main.java
@@ -0,0 +1,13 @@
+package java2022;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int ogrenciSayısı=10;
+		String mesaj="Öğrenci Sayısı: ";
+		System.out.println(mesaj+ ogrenciSayısı);
+
+	}
+
+}
