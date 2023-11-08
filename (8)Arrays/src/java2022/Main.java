5b92797aa12ea43618cd0af6ccaa8c8665d8b611
Eren Beken <erenbeken42@gmail.com>
2023-11-08 21:54:39 +0300

    (8)Arrays

---
+++ (8)Arrays/src/java2022/Main.java
@@ -0,0 +1,23 @@
+package java2022;
+
+public class Main {
+
+	public static void main(String[] args) {
+       	
+		String[] students = new String[4];
+		students[0]= "Steve";
+		students[1]= "Jack";
+	    students[2]= "Abigail";
+	    students[3]= "johann";
+	    
+	    for( int i=0;  i<students.length ;i++ ) {
+	    System.out.println(students[i]);
+	
+	    }
+	
+	
+	
+	
+	
+	}  
+}
