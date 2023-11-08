a4ae979b8051f3e6da5ed7c6ba16362d81733d46
Eren Beken <erenbeken42@gmail.com>
2023-11-08 21:06:21 +0300

    (6)Switch-Case

---
+++ (6)Switch-Case/src/java2022/Main.java
@@ -0,0 +1,33 @@
+package java2022;
+
+public class Main {
+
+	public static void main(String[] args) {
+            char grade ='B';
+            
+            switch(grade) {
+            case 'A':
+            	System.out.println("Perfect");
+            	break;
+            case 'B':
+            case 'C':
+            	System.out.println("Good");
+            	break;
+            	
+            case 'D':
+            	System.out.println("Average");
+            	break;
+            case 'F':
+            	System.out.println("Fail");
+            	break;
+            	default:
+            		System.out.println("Invalid Grade");
+            	
+            		
+		
+		
+		
+		
+	}
+}
+}
