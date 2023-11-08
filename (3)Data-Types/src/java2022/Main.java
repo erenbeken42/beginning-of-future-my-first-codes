8567f3e2e0ee31a33b034add61beeb5b473cba51
Eren Beken <erenbeken42@gmail.com>
2023-11-08 20:33:51 +0300

    (3)Data-Types

---
+++ (3)Data-Types/src/java2022/Main.java
@@ -0,0 +1,24 @@
+package java2022;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int sayi1=11;
+		sayi1=1234;
+		
+		
+		byte sayı2=-23;
+		sayı2=127;
+		
+		
+		double sayı3=12.5;
+		
+		char karakter ='f';
+		
+		boolean dogruMu = true;
+		dogruMu= false;
+		
+	}
+
+}
