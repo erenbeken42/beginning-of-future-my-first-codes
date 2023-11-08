c5f944f4bd097cda1c25a6dda43183cfa85ae1a3
Eren Beken <erenbeken42@gmail.com>
2023-11-08 21:25:22 +0300

    (7)Loops

---
+++ (7)Loops/src/java2022/Main.java
@@ -0,0 +1,28 @@
+package java2022;
+
+public class Main {
+
+	public static void main(String[] args) {
+            
+		for(int i=1;i<5;i++) {
+		System.out.println(i);
+		}
+		
+		int i=8;
+		
+		while(i<7) {
+			System.out.println(i);
+		i+=2;
+		}
+		
+		int j=7;
+		
+      do {
+    	  System.out.println(j);
+		j+=2;
+    	  
+      }while(j<5);
+		//İF CONDİTİONS DONT BE FİT DO WHİLE STİLL RUN ONCE TİME BUT CODE NEVER WORK İN WHİLE 	
+			
+		}  
+}
