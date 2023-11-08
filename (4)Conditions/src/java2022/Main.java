b9477ef9463374f0f4bb729969c784c5cc125b79
Eren Beken <erenbeken42@gmail.com>
2023-11-08 20:38:54 +0300

    (4)Conditions

---
+++ (4)Conditions/src/java2022/Main.java
@@ -0,0 +1,20 @@
+package java2022;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int sayi=45;
+		
+		if(sayi<12) {
+			System.out.println("Sayı 12 den küçük");
+		}else if(sayi==12){
+			System.out.println("Sayı 12 ye eşittir");
+		}else {
+			System.out.println("Sayı 12 den büyüktür");
+		
+	}
+
+	}
+}
+
