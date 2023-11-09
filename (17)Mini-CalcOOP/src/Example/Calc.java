bfa447fe359a67d524f66ac8ca8abd9196b65cd9
Eren Beken <erenbeken42@gmail.com>
2023-11-09 13:06:46 +0300

    (17)Mini-CalcOOP

---
+++ (17)Mini-CalcOOP/src/Example/Calc.java
@@ -0,0 +1,21 @@
+package Example;
+
+public class Calc {
+
+	public int Add(int number1, int number2) {
+		return (number1 + number2);
+	}
+
+	public int Extraction(int number1, int number2) {
+		return (number1 - number2);
+	}
+
+	public int Impact(int number1, int number2) {
+		return(number1*number2);
+	}	
+
+	public int Divide(int number1, int number2) {
+			return(number1/number2);
+	}
+
+	}
---
+++ (17)Mini-CalcOOP/src/Example/Main.java
@@ -0,0 +1,19 @@
+package Example;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number1=10;
+		int number2=5;
+				
+		Calc calc = new Calc();
+		System.out.println(calc.Add(number1, number2));
+		System.out.println(calc.Extraction(number1, number2));
+		System.out.println(calc.Impact(number1, number2));
+		System.out.println(calc.Divide(number1, number2));
+		
+		
+		
+	}
+
+}
