4a341b0b5e92460c87df5929d5e9205c6ddf6a05
Eren Beken <erenbeken42@gmail.com>
2023-11-09 21:25:21 +0300

    (19)BasicInputSyntax

--- (19)BasicInputSyntax/src/Example/Main.java
+++ (19)BasicInputSyntax/src/Example/Main.java
@@ -1,5 +1,7 @@
 package Example;
+
 import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,20 +9,26 @@ public class Main {
 		int s1;
 		int s2;
 		int t;
+        String txt;
+        
+        
 		
 		System.out.println("Enter first integer: ");
-		s1=input.nextInt();
+		s1 = input.nextInt();
 		System.out.println("Enter second integer: ");
-		s2=input.nextInt();
-	    t=s1+s2;
-	    System.out.println("Total="+t);
-	    
-				
-				
+		s2 = input.nextInt();
+		t = s1 + s2;
+		System.out.println("Total=" + t);
+		System.out.println("------------------------------------------");
+
 		
-			
+		Scanner inp = new Scanner(System.in);
+		Example_Class example1 = new Example_Class();
+		example1.Display();
 		
-	
+		System.out.println("Enter Sentence=");
+		txt=inp.nextLine();
+		example1.Display2(txt);	
 
 	}
 
