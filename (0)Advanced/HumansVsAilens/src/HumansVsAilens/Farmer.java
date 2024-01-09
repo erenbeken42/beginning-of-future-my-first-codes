be34866001dc0035f79ad660731acd42793dda87
Eren Beken <119003277+erenbeken42@users.noreply.github.com>
2024-01-10 01:38:45 +0300

    Delete (0)Advanced/HumansVsAilens directory

--- (0)Advanced/HumansVsAilens/src/HumansVsAilens/Farmer.java
+++
@@ -1,9 +0,0 @@
-package HumansVsAilens;
-
-public class Farmer extends Human{
-	public int ProductNumber;
-	public Farmer(int protect,int productNumber) {
-		ProtectHuman=protect;
-		ProductNumber= productNumber;
-	}
-}
